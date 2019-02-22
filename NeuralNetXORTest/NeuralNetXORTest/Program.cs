ffd0ec117103d84451423072b0d077678d420e12
Amitai5 <amitaierfanian25@gmail.com>
2019-02-21 23:27:34 -0800

    Recorded Training Times
    -Displays the amount of time each neural network spends training

--- NeuralNetXORTest/NeuralNetXORTest/Program.cs
+++ NeuralNetXORTest/NeuralNetXORTest/Program.cs
@@ -2,24 +2,23 @@
 using NeuralNetLIB.LearningAlgorithms;
 using NeuralNetLIB.NetworkStructure;
 using System;
-using System.ComponentModel;
 
 namespace NeuralNetXORTest
 {
     class Program
     {
-        //Create Neural Nets
-        static readonly NeuralNetwork ModelNetwork = new NeuralNetwork(new Sigmoid(), 2, 2, 1);
-        static Backpropagation BackpropTrainer = new Backpropagation(ModelNetwork);
-        static Genetics GeneticsTrainer = new Genetics(randy, ModelNetwork, 25);
-        static readonly Random randy = new Random();
-
-        //Neural Net Variables
-        static readonly double NeuralNetworkTargetError = 0.01;
-        static double BackpropError = 0;
-
         static void Main(string[] args)
         {
+            //Create Neural Nets
+            Random randy = new Random();
+            NeuralNetwork ModelNetwork = new NeuralNetwork(new Sigmoid(), 2, 2, 1);
+            Backpropagation BackpropTrainer = new Backpropagation(ModelNetwork);
+            Genetics GeneticsTrainer = new Genetics(randy, ModelNetwork, 25);
+
+            //Neural Net Variables
+            double NeuralNetworkTargetError = 0.01;
+            double BackpropError = 0;
+
             //Set Test Data
             double[][] TestDataOutputs = new double[][]
             {
@@ -39,37 +38,35 @@ namespace NeuralNetXORTest
             //Create Timers
             TimeSpan GeneticsLearnTime = new TimeSpan();
             TimeSpan BackpropLearnTime = new TimeSpan();
-            DateTime StartTime = DateTime.Now;
             Console.CursorVisible = false;
 
             //Train Both At Least Once
             BackpropError = BackpropTrainer.TrainEpoch(TestDataInputs, TestDataOutputs);
             GeneticsTrainer.TrainGeneration(TestDataInputs, TestDataOutputs);
 
-            //Start Background Workers
-            BackgroundWorker GeneticsWorker = new BackgroundWorker();
-            BackgroundWorker BackpropWorker = new BackgroundWorker();
-            GeneticsWorker.DoWork += GeneticsWorker_DoWork;
-            BackpropWorker.DoWork += BackpropWorker_DoWork;
-            GeneticsWorker.RunWorkerAsync();
-            GeneticsWorker.RunWorkerAsync();
-
             while (true)
             {
-                //Train Neural Networks Only Until It Reaches The Goal Error Amount
+                //Train Backprop Neural Network
                 if (BackpropError > NeuralNetworkTargetError)
                 {
-                    BackpropLearnTime = DateTime.Now - StartTime;
+                    DateTime StartTime = DateTime.Now;
+                    BackpropError = BackpropTrainer.TrainEpoch(TestDataInputs, TestDataOutputs);
+                    BackpropLearnTime += DateTime.Now - StartTime;
                 }
+
+                //Train Genetics Neural Network
                 if (GeneticsTrainer.BestNetworkFitness > NeuralNetworkTargetError)
                 {
-                    GeneticsLearnTime = DateTime.Now - StartTime;
+                    DateTime StartTime = DateTime.Now;
+                    GeneticsTrainer.TrainGeneration(TestDataInputs, TestDataOutputs);
+                    GeneticsLearnTime += DateTime.Now - StartTime;
                 }
 
                 //Write Out The Values Of The Backpropagation Neural Network
                 Console.SetCursorPosition(0, 0);
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine($"Backpropagation Results: ");
+                Console.WriteLine($"Epoch Count: {BackpropTrainer.EpochCount}");
                 Console.WriteLine($"Learning Time: {BackpropLearnTime}{Environment.NewLine}");
                 WriteNeuralNetSingleValue(BackpropTrainer.Network, new double[] { 1, 1 });
                 WriteNeuralNetSingleValue(BackpropTrainer.Network, new double[] { 0, 1 });
@@ -85,6 +82,7 @@ namespace NeuralNetXORTest
 
                 //Write Out The Values Of The Genetics Neural Network
                 Console.WriteLine($"Genetics Results: ");
+                Console.WriteLine($"Generation Count: {GeneticsTrainer.GenerationCount}");
                 Console.WriteLine($"Learning Time: {GeneticsLearnTime}{Environment.NewLine}");
                 WriteNeuralNetSingleValue(GeneticsTrainer.BestNetwork, new double[] { 1, 1 });
                 WriteNeuralNetSingleValue(GeneticsTrainer.BestNetwork, new double[] { 0, 1 });
@@ -102,55 +100,6 @@ namespace NeuralNetXORTest
             Console.Write($"({SingleInputValues[0]}, {SingleInputValues[1]}) => ");
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine($"{network.Compute(SingleInputValues)[0]:0.00000}");
-        }
-
-        private static void BackpropWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            //Set Test Data
-            double[][] TestDataOutputs = new double[][]
-            {
-                new double[]{ 0 },
-                new double[]{ 1 },
-                new double[]{ 1 },
-                new double[]{ 0 }
-            };
-            double[][] TestDataInputs = new double[][]
-            {
-                new double[]{ 0, 0 },
-                new double[]{ 1, 0 },
-                new double[]{ 0, 1 },
-                new double[]{ 1, 1 }
-            };
-
-            //Train The Nets
-            while (BackpropError > NeuralNetworkTargetError)
-            {
-                BackpropError = BackpropTrainer.TrainEpoch(TestDataInputs, TestDataOutputs);
-            }
-        }
-        private static void GeneticsWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            //Set Test Data
-            double[][] TestDataOutputs = new double[][]
-            {
-                new double[]{ 0 },
-                new double[]{ 1 },
-                new double[]{ 1 },
-                new double[]{ 0 }
-            };
-            double[][] TestDataInputs = new double[][]
-            {
-                new double[]{ 0, 0 },
-                new double[]{ 1, 0 },
-                new double[]{ 0, 1 },
-                new double[]{ 1, 1 }
-            };
-
-            //Train The Nets
-            while (GeneticsTrainer.BestNetworkFitness > NeuralNetworkTargetError)
-            {
-                GeneticsTrainer.TrainGeneration(TestDataInputs, TestDataOutputs);
-            }
         }
     }
 }
